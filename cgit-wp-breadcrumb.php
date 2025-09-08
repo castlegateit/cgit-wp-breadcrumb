70747562ed5f00bc261cffc60136e9175a7307bb
Andy Reading <andy@castlegateit.co.uk>
2025-09-08 11:04:18 +0100

    Fixed backwards compatibility functions not being included.

--- cgit-wp-breadcrumb.php
+++ cgit-wp-breadcrumb.php
@@ -4,7 +4,7 @@
  * Plugin Name:  Castlegate IT WP Breadcrumb
  * Plugin URI:   https://github.com/castlegateit/cgit-wp-breadcrumb
  * Description:  Simple breadcrumb navigation for WordPress.
- * Version:      3.3.1
+ * Version:      3.3.2
  * Requires PHP: 8.2
  * Author:       Castlegate IT
  * Author URI:   https://www.castlegateit.co.uk/
@@ -16,8 +16,9 @@ if (!defined('ABSPATH')) {
     wp_die('Access denied');
 }
 
-define('CGIT_WP_BREADCRUMB_VERSION', '3.3.1');
+define('CGIT_WP_BREADCRUMB_VERSION', '3.3.2');
 define('CGIT_WP_BREADCRUMB_PLUGIN_FILE', __FILE__);
 define('CGIT_WP_BREADCRUMB_PLUGIN_DIR', __DIR__);
 
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/functions.php';
